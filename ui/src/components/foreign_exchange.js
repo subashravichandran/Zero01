2371bca3b28ac28885c79d66518aaf50d50d836c
Subash R <waytosubash@gmail.com>
2024-03-16 13:54:16 +0530

    fx - final

--- ui/src/components/foreign_exchange.js
+++ ui/src/components/foreign_exchange.js
@@ -4,6 +4,7 @@ import * as k from '../constants'
 
 function ForeignExchange(props) {
   const [fxData, setFxData] = useState([]);
+  const [filteredData, setFilteredData] = useState('');
 
   useEffect(() => {
     FetchAndUpdate(k.FX).then((data) => {
@@ -11,12 +12,35 @@ function ForeignExchange(props) {
     });
   }, []);
 
+  const handleFilter = (event) => {
+    var selectedValue = event.target.value
+    setFilteredData(selectedValue.toUpperCase());
+  }
+
   return(
     <>
       <h2>Foreign Exchange Data</h2>
-      <h3>{fxData.id}</h3>
-      <h3>{fxData.fx_rates['AUD']}</h3>
+      <h3>Todays Data</h3>
+      <input
+        id='curreccy_filter'
+        type="text"
+        value={filteredData}
+        onChange={handleFilter}
+        placeholder="Filter currency..."
+      />
+      {fxData.fx_rates ? FxData({rates: fxData.fx_rates, selected: filteredData}) : 'Loading' }
     </>
+  );
+}
+
+function FxData(props) {
+  return(
+    <ul>
+      { props.selected === '' ?
+          Object.entries(props.rates).map(([currency, rate]) => (
+            <li key={currency}>{currency}: {rate}</li>
+          )) : <li key={props.selected}> {props.selected.toUpperCase()}: {props.rates[props.selected]} </li> }
+    </ul>
   );
 }
 
